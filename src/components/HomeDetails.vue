626e36f5126e8e7d0abaa319fae71d634745fca8
elayudante <juanma@elayudante.es>
2021-10-26 14:25:13 +0200

    añadidas las vistas para homedetails y la imagen de cabecera

---
+++ src/components/HomeDetails.vue
@@ -0,0 +1,62 @@
+<template>
+    <v-container>
+        <v-layout column>
+            <v-flex class="display-2 text-xs-center my-5"
+                >Aquí va un titulazo</v-flex
+            >
+            <v-flex>
+                <div class="headline mt-3">Lorem Ipsum</div>
+                <p class="subheading mt-3">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Nihil eum fugit dolore libero, iste ducimus delectus
+                    voluptas repudiandae, alias vero labore accusantium tempore
+                    aliquam hic. Blanditiis perferendis voluptate adipisci
+                    corporis?
+                </p>
+            </v-flex>
+            <v-flex>
+                <div class="headline mt-3">Lorem ipsum</div>
+                <p class="subheading mt-3">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Quam odit molestiae perspiciatis praesentium architecto eius
+                    vitae consequatur consequuntur dolores, debitis porro
+                    facilis et adipisci quia vero totam aliquam repudiandae
+                    assumenda.
+                </p>
+                <p class="subheading mt-3">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Voluptatibus delectus nisi inventore cum, rerum, facere
+                    eius, ullam repellendus ad porro quo praesentium nam quos
+                    consectetur quis molestias nemo vitae eveniet?
+                </p>
+                <p class="subheading mt-3">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Maiores, perferendis? Natus ullam in voluptatum facilis
+                    culpa nisi eius praesentium. Molestias quam aliquam commodi
+                    sequi praesentium minus veritatis vero labore natus!
+                </p>
+            </v-flex>
+            <v-flex>
+                <div class="headline mt-3">Lorem ipsum</div>
+                <p class="subheading mt-3">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Consequuntur, fugiat sapiente voluptatum numquam eos ducimus
+                    dolorum distinctio hic ratione architecto ut porro
+                    exercitationem placeat rerum itaque sed vel doloremque
+                    labore.
+                </p>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'HomeDetails',
+}
+</script>
+
+<style scoped>
+
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,16 @@
 <template>
-    
+    <span>
+        <home-hero></home-hero>
+    </span>
 </template>
 
 <script>
+import HomeHero from '@/components/HomeHero';
 // @ is an alias to /src
 export default {
     name: 'Home',
+    components: {
+        HomeHero,
+    }
 };
 </script>
